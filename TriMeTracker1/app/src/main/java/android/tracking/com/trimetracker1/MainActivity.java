54d0f9cbb8e276ddfd05e2f3953f274ef280f4ba
jhen <jhennelynsarmiento@gmail.com>
2019-02-10 00:55:31 +0800

    Registration (with authentication to firebase) and login page (design)

--- TriMeTracker1/app/src/main/java/android/tracking/com/trimetracker1/MainActivity.java
+++ TriMeTracker1/app/src/main/java/android/tracking/com/trimetracker1/MainActivity.java
@@ -5,14 +5,45 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 private Button btnwelcome;
+
+
+    private EditText Email;
+    private EditText Password;
+    private Button Login;
+    private TextView userRegister;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Email = (EditText)findViewById(R.id.userEmail);
+        Password = (EditText)findViewById(R.id.userPassword);
+        Login = (Button)findViewById(R.id.btnUserLogin);
+        userRegister = (TextView)findViewById(R.id.tvRegister);
+
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validate(Email.getText().toString(), Password.getText().toString());
+            }
+        });
+
+        userRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, RegistrationActivity.class));
+            }
+        });
 
         btnwelcome = (Button)findViewById(R.id.btnWelcome);
         btnwelcome.setOnClickListener(new View.OnClickListener() {
@@ -22,5 +53,15 @@ private Button btnwelcome;
                 startActivity(intent);
             }
         });
+    }
+    private  void validate(String userEmail, String userPassword){
+        if((userEmail.equals("Admin")) && (userPassword.equals("1234"))){
+            Intent intent = new Intent(MainActivity.this, HomePage.class);
+            startActivity(intent);
+        }else{
+            Toast toast = Toast.makeText(getApplicationContext(),"Incorrect Email or Password!", Toast.LENGTH_LONG);
+            toast.show();
+
+        }
     }
 }
