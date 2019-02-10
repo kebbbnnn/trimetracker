85c06cca32c0b8c42a629d8e29169a44d3a328ca
kebbbnnn <kevinladan@outlook.com>
2019-02-11 02:18:23 +0800

    Save location to firebase db

---
+++ TriMeTracker1/app/src/main/java/android/tracking/com/trimetracker1/data/LocationData.java
@@ -0,0 +1,27 @@
+package android.tracking.com.trimetracker1.data;
+
+import com.google.firebase.database.Exclude;
+
+public class LocationData {
+    @Exclude
+    public long id;
+    public String userId;
+    public double lat;
+    public double lng;
+    public long timestamp = 0L;
+
+    public LocationData(String userId, double lat, double lng, long timestamp) {
+        this.userId = userId;
+        this.lat = lat;
+        this.lng = lng;
+        this.timestamp = timestamp;
+    }
+
+    public LocationData(String userId, double lat, double lng) {
+        this.userId = userId;
+        this.lat = lat;
+        this.lng = lng;
+        this.timestamp = this.timestamp == 0L ? System.currentTimeMillis() : this.timestamp;
+    }
+
+}
