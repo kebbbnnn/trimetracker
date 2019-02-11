f5c48604c3f1c8f65b7b0a6cea71393b38b71cf6
jhen <jhennelynsarmiento@gmail.com>
2019-02-11 23:23:14 +0800

    edit

--- TriMeTracker1/app/src/main/java/android/tracking/com/trimetracker1/MainActivity.java
+++ TriMeTracker1/app/src/main/java/android/tracking/com/trimetracker1/MainActivity.java
@@ -17,6 +17,7 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+
 public class MainActivity extends AppCompatActivity {
 private Button btnwelcome;
 
