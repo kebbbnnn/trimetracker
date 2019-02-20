5696d10bd5533ae6235ed629c2254b99b1bfc1b5
kebbbnnn <kevinladan@outlook.com>
2019-02-20 17:27:17 +0800

    Fixed tag reader crash

--- TriMeTracker1/app/src/main/java/android/tracking/com/trimetracker1/TagReader.java
+++ TriMeTracker1/app/src/main/java/android/tracking/com/trimetracker1/TagReader.java
@@ -4,49 +4,43 @@ import android.content.Intent;
 import android.nfc.NdefMessage;
 import android.nfc.NfcAdapter;
 import android.nfc.Tag;
-import android.nfc.tech.IsoDep;
-import android.nfc.tech.MifareClassic;
-import android.nfc.tech.MifareUltralight;
-import android.nfc.tech.Ndef;
-import android.nfc.tech.NdefFormatable;
-import android.nfc.tech.NfcA;
-import android.nfc.tech.NfcB;
-import android.nfc.tech.NfcF;
-import android.nfc.tech.NfcV;
+import android.nfc.tech.*;
 import android.os.Parcelable;
 
 
-    public class TagReader {
-        private static final String[] TECHES = new String[]{
-                NfcA.class.getName(), NfcB.class.getName(), NfcF.class.getName(), NfcV.class.getName(),
-                IsoDep.class.getName(), Ndef.class.getName(), NdefFormatable.class.getName(), MifareClassic.class.getName(), MifareUltralight.class.getName()};
+public class TagReader {
+    private static final String[] TECHES = new String[]{
+            NfcA.class.getName(), NfcB.class.getName(), NfcF.class.getName(), NfcV.class.getName(),
+            IsoDep.class.getName(), Ndef.class.getName(), NdefFormatable.class.getName(), MifareClassic.class.getName(), MifareUltralight.class.getName()};
 
-        public static String readTag(Tag tag, Intent intent) {
-            if (tag != null) {
-                StringBuilder stringBuilder = new StringBuilder();
-                String[] tagTechList = tag.getTechList();
-                if (tagTechList != null) {
-                    for (int i = 0; i < tagTechList.length; i++) {
-                       stringBuilder.append(readTech(tag, tagTechList[i],intent));
-                    }
-                }
-                return stringBuilder.toString();
-            }
-            return null;
-        }
-
-        private static String readTech(Tag tag, String curTagTech,Intent intent) {
+    public static String readTag(Tag tag, Intent intent) {
+        if (tag != null) {
             StringBuilder stringBuilder = new StringBuilder();
-            if (TECHES[5].equals(curTagTech)) {
-                Ndef ndefTag = Ndef.get(tag);
-                Parcelable[] rawsMsgs = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
-                NdefMessage msg= (NdefMessage) rawsMsgs[0];
-                stringBuilder.append(new String(msg.getRecords()[0].getPayload()));
+            String[] tagTechList = tag.getTechList();
+            if (tagTechList != null) {
+                for (int i = 0; i < tagTechList.length; i++) {
+                    stringBuilder.append(readTech(tag, tagTechList[i], intent));
+                }
             }
             return stringBuilder.toString();
         }
-
+        return null;
     }
+
+    private static String readTech(Tag tag, String curTagTech, Intent intent) {
+        StringBuilder stringBuilder = new StringBuilder();
+        if (TECHES[5].equals(curTagTech)) {
+            Ndef ndefTag = Ndef.get(tag);
+            Parcelable[] rawsMsgs = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+            if (rawsMsgs != null) {
+                NdefMessage msg = (NdefMessage) rawsMsgs[0];
+                stringBuilder.append(new String(msg.getRecords()[0].getPayload()));
+            }
+        }
+        return stringBuilder.toString();
+    }
+
+}
 
 
 
