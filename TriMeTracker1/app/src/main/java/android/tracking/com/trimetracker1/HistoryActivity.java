b5510b524528da7e0cc5ee09fc6537bbc9fa04d9
kebbbnnn <kevinladan@outlook.com>
2019-02-24 00:57:37 +0800

    History page

--- TriMeTracker1/app/src/main/java/android/tracking/com/trimetracker1/HistoryActivity.java
+++ TriMeTracker1/app/src/main/java/android/tracking/com/trimetracker1/HistoryActivity.java
@@ -11,10 +11,14 @@ import android.tracking.com.trimetracker1.data.LocationList;
 import android.tracking.com.trimetracker1.data.Message;
 import android.view.MenuItem;
 import com.crashlytics.android.Crashlytics;
+import com.google.android.gms.maps.GoogleMap;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.*;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class HistoryActivity extends AppCompatActivity {
 
@@ -23,6 +27,9 @@ public class HistoryActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private int data_size = 0, counter = 0;
 
+    private SessionDataListener eventListener = new SessionDataListener();
+    private LocationDataListener locListener = new LocationDataListener();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,15 +37,47 @@ public class HistoryActivity extends AppCompatActivity {
         recyclerView = findViewById(R.id.recyclerview);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(adapter);
+        recyclerView.setRecyclerListener(mRecycleListener);
 
-        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setTitle(R.string.history);
+        }
 
         String currentUserId = FirebaseAuth.getInstance().getCurrentUser().getUid();
         DatabaseReference eventRef = FirebaseDatabase.getInstance().getReference().child("messages");
         eventRef.orderByChild("receiverId").equalTo(currentUserId).addListenerForSingleValueEvent(eventListener);
     }
 
-    private ValueEventListener eventListener = new ValueEventListener() {
+    /**
+     * RecycleListener that completely clears the {@link com.google.android.gms.maps.GoogleMap}
+     * attached to a row in the RecyclerView.
+     * Sets the map type to {@link com.google.android.gms.maps.GoogleMap#MAP_TYPE_NONE} and clears
+     * the map.
+     */
+    private RecyclerView.RecyclerListener mRecycleListener = holder -> {
+        HistoryAdapter.ViewHolder mapHolder = (HistoryAdapter.ViewHolder) holder;
+        if (mapHolder != null && mapHolder.googleMap != null) {
+            // Clear the map and free up resources by changing the map type to none.
+            // Also reset the map when it gets reattached to layout, so the previous map would
+            // not be displayed.
+            mapHolder.googleMap.clear();
+            mapHolder.googleMap.setMapType(GoogleMap.MAP_TYPE_NONE);
+        }
+    };
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            onBackPressed();
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private class SessionDataListener implements ValueEventListener {
 
         @Override
         public void onDataChange(@NonNull DataSnapshot snapshot) {
@@ -53,7 +92,7 @@ public class HistoryActivity extends AppCompatActivity {
                     DatabaseReference locRef = FirebaseDatabase.getInstance().getReference().child("locations");
                     locRef.orderByChild("sessionId")
                             .equalTo(msg.getSessionId())
-                            .addListenerForSingleValueEvent(locListener);
+                            .addListenerForSingleValueEvent(locListener.sender(msg.getSenderName()));
                 }
             }
         }
@@ -62,9 +101,15 @@ public class HistoryActivity extends AppCompatActivity {
         public void onCancelled(@NonNull DatabaseError databaseError) {
 
         }
-    };
+    }
 
-    private ValueEventListener locListener = new ValueEventListener() {
+    private class LocationDataListener implements ValueEventListener {
+        private String sender;
+
+        LocationDataListener sender(String sender) {
+            this.sender = sender;
+            return this;
+        }
 
         @Override
         public void onDataChange(@NonNull DataSnapshot snapshot) {
@@ -74,17 +119,12 @@ public class HistoryActivity extends AppCompatActivity {
                 //@formatter:on
                 Map<String, LocationData> map = snapshot.getValue(objectsGTypeInd);
                 ArrayList<LocationData> list = new ArrayList<>(map.values());
-                locationDataList.add(new LocationList(list));
-                counter++;
-                if (data_size == counter) {
-                    adapter.setLocationDataList(locationDataList);
-//                    for (int i = 0, size = locationDataList.size(); i < size; i++) {
-//                        LocationList data = locationDataList.get(i);
-//                        Log.e("test", "list [" + i + "]");
-//                        for (LocationData loc : data.data) {
-//                            Log.e("test", "loc sessionId: " + loc.sessionId);
-//                        }
-//                    }
+                if (!list.isEmpty()) {
+                    locationDataList.add(new LocationList(this.sender, list));
+                    counter++;
+                    if (data_size == counter) {
+                        adapter.setLocationDataList(locationDataList);
+                    }
                 }
             } catch (Exception e) {
                 e.printStackTrace();
@@ -95,17 +135,6 @@ public class HistoryActivity extends AppCompatActivity {
         @Override
         public void onCancelled(@NonNull DatabaseError databaseError) {
 
-        }
-    };
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if (id == android.R.id.home) {
-            onBackPressed();
-            return true;
-        } else {
-            return super.onOptionsItemSelected(item);
         }
     }
 }
--- TriMeTracker1/app/src/main/java/android/tracking/com/trimetracker1/adapter/HistoryAdapter.java
+++ TriMeTracker1/app/src/main/java/android/tracking/com/trimetracker1/adapter/HistoryAdapter.java
@@ -6,25 +6,20 @@ import android.os.Looper;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.tracking.com.trimetracker1.R;
+import android.tracking.com.trimetracker1.Session;
+import android.tracking.com.trimetracker1.data.LocationData;
 import android.tracking.com.trimetracker1.data.LocationList;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import com.mapbox.api.directions.v5.DirectionsCriteria;
-import com.mapbox.api.directions.v5.MapboxDirections;
-import com.mapbox.api.directions.v5.models.DirectionsResponse;
-import com.mapbox.api.directions.v5.models.DirectionsRoute;
-import com.mapbox.api.directions.v5.models.DirectionsWaypoint;
-import com.mapbox.mapboxsdk.Mapbox;
-import com.mapbox.mapboxsdk.maps.MapView;
-import com.mapbox.mapboxsdk.maps.MapboxMap;
-import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
-import com.mapbox.mapboxsdk.maps.Style;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
+import android.widget.TextView;
+import com.google.android.gms.maps.*;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class HistoryAdapter extends RecyclerView.Adapter<HistoryAdapter.ViewHolder> {
@@ -33,194 +28,59 @@ public class HistoryAdapter extends RecyclerView.Adapter<HistoryAdapter.ViewHold
 
     private List<LocationList> locationDataList;
 
-    class ViewHolder extends RecyclerView.ViewHolder implements OnMapReadyCallback {
-        MapView mapView;
-        MapboxMap mapboxMap;
+    public class ViewHolder extends RecyclerView.ViewHolder implements OnMapReadyCallback {
+        private MapView mapView;
+        public GoogleMap googleMap;
+        private TextView textSender;
 
         public ViewHolder(View itemView) {
             super(itemView);
-            mapView = itemView.findViewById(R.id.mapView);
+            mapView = itemView.findViewById(R.id.map);
             mapView.onCreate(null);
+            mapView.getMapAsync(this);
+            textSender = itemView.findViewById(R.id.textSenderName);
         }
 
         @Override
-        public void onMapReady(@NonNull MapboxMap mapboxMap) {
-            this.mapboxMap = mapboxMap;
-            this.mapboxMap.getUiSettings().setAllGesturesEnabled(false);
-            this.mapboxMap.setStyle(Style.DARK);
+        public void onMapReady(GoogleMap googleMap) {
+            MapsInitializer.initialize(Session.getInstance());
+            this.googleMap = googleMap;
+            this.googleMap.getUiSettings().setAllGesturesEnabled(false);
+            this.googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
             setMapLocation();
         }
 
         private void bind(int position) {
             LocationList loc = locationDataList.get(position);
             mapView.setTag(loc);
+            textSender.setText(loc.senderName);
             setMapLocation();
         }
 
         private void setMapLocation() {
             try {
-                if (mapboxMap == null) return;
+                if (this.googleMap == null) return;
 
                 LocationList data = (LocationList) mapView.getTag();
                 if (data == null) return;
 
-                //              List<LatLng> path = new ArrayList<>();
+                List<LatLng> path = new ArrayList<>();
+                for (LocationData loc : data.data) {
+                    LatLng latLng = new LatLng(loc.lat, loc.lng);
+                    this.googleMap.addMarker(new MarkerOptions().position(latLng));
+                    path.add(latLng);
+                }
 
+                LatLngBounds.Builder builder = new LatLngBounds.Builder();
+                for (LatLng latLng : path) {
+                    builder.include(latLng);
+                }
 
-                //Execute Directions API request
-//                GeoApiContext context = new GeoApiContext.Builder()
-//                        .apiKey("AIzaSyA41i7F9Hw7fGCvWnTNUKpBz2fwQOsnUlE")
-//                        .build();
-//
-//                LocationData locOrigin = data.data.get(0);
-//                String origin = locOrigin.lat + "," + locOrigin.lng;
-//                LocationData locDestination = data.data.get(data.data.size() - 1);
-//                String destination = locDestination.lat + "," + locDestination.lng;
-//                Log.e("test", "origin: " + origin + ", destination: " + destination);
+                final LatLngBounds bounds = builder.build();
 
-                DirectionsWaypoint origin = DirectionsWaypoint.builder().rawLocation(new double[]{-77.04341, 38.90962}).build();
-
-                DirectionsWaypoint destination = DirectionsWaypoint.builder().rawLocation(new double[]{-77.0365, 38.8977}).build();
-
-                MapboxDirections client = MapboxDirections.builder()
-                        .accessToken("pk.eyJ1Ijoia2ViYmJubm4iLCJhIjoiY2pzZjBkaGl0MG8wZTQzczdvM2Fmbm1vNiJ9.C4e_NkvKOhjRvq-gDE1JdQ")
-                        .origin(origin.location())
-                        .destination(destination.location())
-                        .profile(DirectionsCriteria.PROFILE_WALKING)
-                        .build();
-
-                client.enqueueCall(new Callback<DirectionsResponse>() {
-                    @Override
-                    public void onResponse(Call<DirectionsResponse> call, Response<DirectionsResponse> response) {
-//                        List<LatLng> path = new ArrayList<>();
-//                        try {
-//                            List<DirectionsRoute> routes = response.body().routes();
-//                            //Loop through legs and steps to get encoded polylines of each step
-//                            if (routes != null && routes.size() > 0) {
-//                                DirectionsRoute route = routes.get(0);
-//                                if (route.legs() != null) {
-//                                    for (int i = 0; i < route.legs().size(); i++) {
-//                                        RouteLeg leg = route.legs().get(i);
-//                                        if (leg.steps() != null) {
-//                                            for (int j = 0; j < leg.steps().size(); j++) {
-//                                                LegStep step = leg.steps().get(j);
-//                                                if (step.intersections() != null && step.intersections().size() > 0) {
-//                                                    for (int k = 0; k < step.intersections().size(); k++) {
-//                                                        StepIntersection stepIntersection = step.intersections().get(k);
-//                                                        Point point = stepIntersection.location();
-//                                                        path.add(new LatLng(point.latitude(), point.longitude()));
-//                                                    }
-//                                                } else {
-//                                                    Point point = step.maneuver().location();
-//                                                    path.add(new LatLng(point.latitude(), point.longitude()));
-//                                                }
-//                                            }
-//                                        }
-//                                    }
-//                                }
-//                            }
-//                        } catch (Exception ex) {
-//                            Log.e("test", ex.getLocalizedMessage());
-//                        }
-//
-//                        //Draw the polyline
-//                        if (path.size() > 0) {
-//                            PolylineOptions opts = new PolylineOptions().addAll(path).color(Color.BLUE).width(5);
-//                            mapboxMap.addPolyline(opts);
-//                        }
-//
-//                        LatLngBounds.Builder builder = new LatLngBounds.Builder();
-//                        for (LatLng latLng : path) {
-//                            builder.include(latLng);
-//                        }
-//
-//                        final LatLngBounds bounds = builder.build();
-//
-//                        //BOUND_PADDING is an int to specify padding of bound.. try 100.
-//                        CameraUpdate cu = CameraUpdateFactory.newLatLngBounds(bounds, BOUND_PADDING);
-//                        mapboxMap.animateCamera(cu);
-
-
-                        if (response.body() == null) {
-                            Log.e("test", "No routes found, make sure you set the right user and access token.");
-                            return;
-                        } else if (response.body().routes().size() < 1) {
-                            Log.e("test", "No routes found");
-                            return;
-                        }
-
-                        // Retrieve the directions route from the API response
-                        DirectionsRoute currentRoute = response.body().routes().get(0);
-                    }
-
-                    @Override
-                    public void onFailure(Call<DirectionsResponse> call, Throwable t) {
-
-                    }
-
-                });
-
-//                DirectionsApiRequest req = DirectionsApi.getDirections(context, origin, destination);
-//                try {
-//                    DirectionsResult res = req.await();
-//
-//                    //Loop through legs and steps to get encoded polylines of each step
-//                    if (res.routes != null && res.routes.length > 0) {
-//                        DirectionsRoute route = res.routes[0];
-//
-//                        if (route.legs != null) {
-//                            for (int i = 0; i < route.legs.length; i++) {
-//                                DirectionsLeg leg = route.legs[i];
-//                                if (leg.steps != null) {
-//                                    for (int j = 0; j < leg.steps.length; j++) {
-//                                        DirectionsStep step = leg.steps[j];
-//                                        if (step.steps != null && step.steps.length > 0) {
-//                                            for (int k = 0; k < step.steps.length; k++) {
-//                                                DirectionsStep step1 = step.steps[k];
-//                                                EncodedPolyline points1 = step1.polyline;
-//                                                if (points1 != null) {
-//                                                    //Decode polyline and add points to list of route coordinates
-//                                                    List<com.google.maps.model.LatLng> coords1 = points1.decodePath();
-//                                                    for (com.google.maps.model.LatLng coord1 : coords1) {
-//                                                        path.add(new LatLng(coord1.lat, coord1.lng));
-//                                                    }
-//                                                }
-//                                            }
-//                                        } else {
-//                                            EncodedPolyline points = step.polyline;
-//                                            if (points != null) {
-//                                                //Decode polyline and add points to list of route coordinates
-//                                                List<com.google.maps.model.LatLng> coords = points.decodePath();
-//                                                for (com.google.maps.model.LatLng coord : coords) {
-//                                                    path.add(new LatLng(coord.lat, coord.lng));
-//                                                }
-//                                            }
-//                                        }
-//                                    }
-//                                }
-//                            }
-//                        }
-//                    }
-//                } catch (Exception ex) {
-//                    Log.e("test", ex.getLocalizedMessage());
-//                }
-//
-//                //Draw the polyline
-//                if (path.size() > 0) {
-//                    PolylineOptions opts = new PolylineOptions().addAll(path).color(Color.BLUE).width(5);
-//                    map.addPolyline(opts);
-//                }
-//
-//                LatLngBounds.Builder builder = new LatLngBounds.Builder();
-//                for (LatLng latLng : path) {
-//                    builder.include(latLng);
-//                }
-//
-//                final LatLngBounds bounds = builder.build();
-//
-//                //BOUND_PADDING is an int to specify padding of bound.. try 100.
-//                CameraUpdate cu = CameraUpdateFactory.newLatLngBounds(bounds, BOUND_PADDING);
-//                map.animateCamera(cu);
+                //BOUND_PADDING is an int to specify padding of bound.. try 100.
+                CameraUpdate cu = CameraUpdateFactory.newLatLngBounds(bounds, BOUND_PADDING);
+                this.googleMap.animateCamera(cu);
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -236,11 +96,11 @@ public class HistoryAdapter extends RecyclerView.Adapter<HistoryAdapter.ViewHold
         super();
     }
 
+    @NonNull
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         final Context context = parent.getContext();
-        Mapbox.getInstance(context, context.getString(R.string.mapbox_access_token));
-        final LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        final LayoutInflater inflater = LayoutInflater.from(context);
         try {
             View root = inflater.inflate(R.layout.item_history, parent, false);
             return new ViewHolder(root);
