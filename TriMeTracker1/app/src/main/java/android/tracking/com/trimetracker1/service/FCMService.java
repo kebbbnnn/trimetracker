b0dce0f2934ebd9c468d1cda87677aa56dc9f4f4
kebbbnnn <kevinladan@outlook.com>
2019-02-25 14:38:17 +0800

    Replaced Notification with NotificationCompat

--- TriMeTracker1/app/src/main/java/android/tracking/com/trimetracker1/service/FCMService.java
+++ TriMeTracker1/app/src/main/java/android/tracking/com/trimetracker1/service/FCMService.java
@@ -1,12 +1,12 @@
 package android.tracking.com.trimetracker1.service;
 
-import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.media.RingtoneManager;
 import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
 import android.tracking.com.trimetracker1.R;
 import android.tracking.com.trimetracker1.TrackActivity;
 import android.util.Log;
@@ -69,7 +69,7 @@ public class FCMService extends FirebaseMessagingService {
                 PendingIntent.FLAG_ONE_SHOT);
 
         Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-        Notification.Builder notificationBuilder = new Notification.Builder(this)
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
                 .setSmallIcon(R.mipmap.ic_launcher)
                 .setContentTitle(notificationTitle)
                 .setContentText(notificationBody)
